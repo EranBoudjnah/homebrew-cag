f99ca6bd88a0f2a15e5354e9d5e44a19f952fcd9
Eran Boudjnah <eranbou@gmail.com>
2025-10-06 16:19:37 +0100

    v0.0.2

---
+++ Formula/clean_architecture_generator.rb
@@ -0,0 +1,20 @@
+class CleanArchitectureGenerator < Formula
+  desc "Generate clean architecture boilerplate for Kotlin"
+  homepage "https://github.com/EranBoudjnah/CleanArchitectureGenerator"
+  url "https://github.com/EranBoudjnah/CleanArchitectureGenerator/releases/download/cli-v0.0.2/cag-0.0.2.tar"
+  sha256 "f6b0e15ce15abb7f78521bc3f2e659bd014d38c551075ed52ffee3283d646214"
+  license "MIT"
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install Dir["lib/*"]
+    bin.install "bin/cag"
+    bin.install "bin/cag.bat"
+  end
+
+  test do
+    assert_match "0.0.2", shell_output("#{bin}/cag --version")
+  end
+end
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# EranBoudjnah Cag
+
+## How do I install these formulae?
+
+`brew install eranboudjnah/cag/<formula>`
+
+Or `brew tap eranboudjnah/cag` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "eranboudjnah/cag"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
