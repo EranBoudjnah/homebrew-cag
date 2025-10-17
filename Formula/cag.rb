f6dfe8ce53cb1cb027bf0a151338b99e1c61f655
Eran Boudjnah <eranbou@gmail.com>
2025-10-17 13:35:35 +0100

    Updated to version 0.4.0.

--- Formula/cag.rb
+++ Formula/cag.rb
@@ -1,8 +1,8 @@
 class Cag < Formula
   desc "Generate clean architecture boilerplate for Kotlin"
   homepage "https://github.com/EranBoudjnah/CleanArchitectureGenerator"
-  url "https://github.com/EranBoudjnah/CleanArchitectureGenerator/releases/download/v0.3.0/cag-0.3.0.tar"
-  sha256 "7c423e32e4aa0ca319ebadcd3f8506f45c97d343ddc3bd632a1c581d18c7d300"
+  url "https://github.com/EranBoudjnah/CleanArchitectureGenerator/releases/download/v0.4.0/cag-0.4.0.tar"
+  sha256 "f4aeeb452c48ddc39b03cd1bf53a8d92bf8392d864d649fdc88adcecb6f3b288"
   license "MIT"
 
   depends_on "openjdk"
